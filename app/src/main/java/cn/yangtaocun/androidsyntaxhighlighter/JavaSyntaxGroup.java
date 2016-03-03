999eccd9b1840bab5619ad98688133621cfaf563
yw981 <yw981@sina.com>
2016-03-03 17:19:20 +0800

    second commit

--- app/src/main/java/cn/yangtaocun/androidsyntaxhighlighter/JavaSyntaxGroup.java
+++ app/src/main/java/cn/yangtaocun/androidsyntaxhighlighter/JavaSyntaxGroup.java
@@ -3,13 +3,23 @@ package cn.yangtaocun.androidsyntaxhighlighter;
 import android.graphics.Color;
 
 /**
- * Created by dell on 2016/3/2.
+ * Default Java Syntax Highlighter
+ * Created by Tony Yang on 2016/3/2.
  */
 public class JavaSyntaxGroup extends SyntaxHighlighter {
     @Override
     protected void initSyntaxElements() {
-        addSyntaxElement(new SyntaxElement("\\b([A-Z]\\w*)",Color.GREEN));
-        addSyntaxElement(new SyntaxElement("\\b([a-z]\\w*)",Color.YELLOW));
-        addSyntaxElement(new SyntaxElement("\\b(public|super)", Color.RED));
+        // Classes
+        addSyntaxElement(new SyntaxElement("\\b([A-Z]\\w*)",Color.rgb(118,0,0)));
+        // Functions
+        addSyntaxElement(new SyntaxElement("\\b([a-z]\\w*)",Color.rgb(158,72,0)));
+        // Keywords
+        addSyntaxElement(new SyntaxElement("\\b(public|super|int|float)", Color.rgb(46,0,118)));
+        // Values
+        addSyntaxElement(new SyntaxElement("\\b\".*\"\\s", Color.rgb(0,118,0)));
+        addSyntaxElement(new SyntaxElement("\\b(-?\\\\d+)(\\\\.\\\\d+)?\\s", Color.rgb(0,118,0)));
+        // Comments
+        addSyntaxElement(new SyntaxElement("\\b\\\\.*", Color.rgb(118,118,118)));
+        addSyntaxElement(new SyntaxElement("\\b/\\*.*\\*/\\s", Color.rgb(118,118,118)));
     }
 }
